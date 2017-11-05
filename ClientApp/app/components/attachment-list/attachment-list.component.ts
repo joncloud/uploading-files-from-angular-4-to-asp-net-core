d9ff39c4fa503332fe006ff39249edbeed614ea9
joncloud <jdberube@gmail.com>
2017-11-05 13:53:51 -0800

    Updates client upload to use file input.

--- ClientApp/app/components/attachment-list/attachment-list.component.ts
+++ ClientApp/app/components/attachment-list/attachment-list.component.ts
@@ -4,19 +4,30 @@ import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'attachment-list',
+    styleUrls: ['./attachment-list.component.css'],
     templateUrl: './attachment-list.component.html'
 })
 export class AttachmentListComponent {
     constructor(private readonly httpClient: HttpClient) { }
 
-    upload() {
-        const contents = new Blob('Lorem ipsum dolor sit amet'.split(''));
+    upload(event: Event) {
+        const inputElement = <HTMLInputElement>(event.srcElement || event.target);
+        const files = inputElement.files;
+
+        if (!files) return;
+
         const formData = new FormData();
-        formData.set('file', contents, 'contents.txt');
+        for (var index = 0; index < files.length; index++) {
+            const file = files[index];
+            formData.set(file.name, file, file.name);
+        }
 
         this.httpClient.post<{contents: string}>('upload', formData)
             .subscribe(
                 response => console.log(response),
                 error => console.log(error));
+
+        // Clear the value to allow uploading the same file again.
+        inputElement.value = '';
     }
 }
